1e4272afbe48963f2e71b9afc47c6d94ecc27184
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-01-24 10:52:36 +0530

    import java.io.*; import java.util.*; public class frames{     public static void main(String args[]){         Scanner t=new Scanner(System.in);         int a=t.nextInt();         int b=t.nextInt();         int c=t.nextInt();         System.out.println((2*(a+b))*c);     } }

---
+++ Frames.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class frames{
+    public static void main(String args[]){
+        Scanner t=new Scanner(System.in);
+        int a=t.nextInt();
+        int b=t.nextInt();
+        int c=t.nextInt();
+        System.out.println((2*(a+b))*c);
+    }
+}
