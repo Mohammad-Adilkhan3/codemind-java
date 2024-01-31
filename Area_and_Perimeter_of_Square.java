bb3b8bdfad545a80017621bcfecfb32d0fad7bfd
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-01-31 10:10:37 +0530

    import java.io.*; import java.util.*; public class lp{     public static void main(String args[]){         Scanner t=new Scanner(System.in);         int x=t.nextInt();         int a=x*x;         int p=4*x;         System.out.print(a+" ");         System.out.print(p);     } }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class lp{
+    public static void main(String args[]){
+        Scanner t=new Scanner(System.in);
+        int x=t.nextInt();
+        int a=x*x;
+        int p=4*x;
+        System.out.print(a+" ");
+        System.out.print(p);
+    }
+}
