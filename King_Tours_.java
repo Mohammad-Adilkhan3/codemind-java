3b9a9d7b87b371f3262e4a915ed3c9ce73d6025b
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-01-31 10:01:22 +0530

    import java.io.*; import java.util.*; public class lp{     public static void main(String args[]){         Scanner t=new Scanner(System.in);         int x=t.nextInt();         int y=t.nextInt();         System.out.println((x*5)+(y*7));     } }

---
+++ King_Tours_.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class lp{
+    public static void main(String args[]){
+        Scanner t=new Scanner(System.in);
+        int x=t.nextInt();
+        int y=t.nextInt();
+        System.out.println((x*5)+(y*7));
+    }
+}
