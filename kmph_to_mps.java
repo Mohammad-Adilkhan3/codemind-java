7aa080090ac7e1df276623cf1cc688cfb446b7c7
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-02-07 10:20:25 +0530

    import java.io.*; import java.util.*; public class sam{     public static void main(String args[]){         Scanner t=new Scanner(System.in);         int a=t.nextInt();         double res=a*0.277778;         System.out.printf("%.2f",res);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class sam{
+    public static void main(String args[]){
+        Scanner t=new Scanner(System.in);
+        int a=t.nextInt();
+        double res=a*0.277778;
+        System.out.printf("%.2f",res);
+    }
+}
