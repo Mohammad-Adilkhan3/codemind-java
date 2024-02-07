4416f91842c37cb7a1a577fd416d20bc66ff3b99
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-02-07 09:52:05 +0530

    import java.io.*; import java.util.*; public class sam{     public static void main(String args[]){         Scanner t=new Scanner(System.in);         int a=t.nextInt();         int b=t.nextInt();         System.out.println(2*a-b);     } }

---
+++ Find_Second_Number.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class sam{
+    public static void main(String args[]){
+        Scanner t=new Scanner(System.in);
+        int a=t.nextInt();
+        int b=t.nextInt();
+        System.out.println(2*a-b);
+    }
+}
