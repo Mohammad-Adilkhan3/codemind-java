9ab62b7dc385c5633f5eefc69eab4de707f0781b
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-02-07 10:25:21 +0530

    import java.io.*; import java.util.*; public class sam{     public static void main(String args[]){         Scanner t=new Scanner(System.in);         String a=t.next();         int res=Integer.parseInt(a,8);         System.out.println(res);     } }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class sam{
+    public static void main(String args[]){
+        Scanner t=new Scanner(System.in);
+        String a=t.next();
+        int res=Integer.parseInt(a,8);
+        System.out.println(res);
+    }
+}
