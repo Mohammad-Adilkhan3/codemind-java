962cdc6fec5d5bed83afc99898b4f271dbffa094
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-02-14 10:05:43 +0530

    import java.io.*; import java.util.*; public class sam{     public static void main(String args[]){         Scanner t=new Scanner(System.in);         int T=t.nextInt();         System.out.printf("%.2f",T*2.54);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class sam{
+    public static void main(String args[]){
+        Scanner t=new Scanner(System.in);
+        int T=t.nextInt();
+        System.out.printf("%.2f",T*2.54);
+    }
+}
