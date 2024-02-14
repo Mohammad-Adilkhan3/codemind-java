489a47fa91e44fb7fbc38dbfaccdb33a06434f63
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-02-14 10:14:40 +0530

    import java.io.*; import java.util.*; public class sam{     public static void main(String args[]){         Scanner t=new Scanner(System.in);         int T=t.nextInt();         int x=t.nextInt();         int y=t.nextInt();         System.out.println((y-x)/T);         }     }

---
+++ Can_Cross_the_Bridge.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class sam{
+    public static void main(String args[]){
+        Scanner t=new Scanner(System.in);
+        int T=t.nextInt();
+        int x=t.nextInt();
+        int y=t.nextInt();
+        System.out.println((y-x)/T);
+        }
+    }
