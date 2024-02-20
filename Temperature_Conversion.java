825009939ea90cc40c2262eea62bf7d8f7c3aa42
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-02-20 10:17:15 +0530

    import java.io.*; import java.util.*; public class sam{     public static void main(String args[]){         Scanner t=new Scanner(System.in);         int b=t.nextInt();         double res=32+(b*1.8);         System.out.format("%.2f ",res);
        }
    }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class sam{
+    public static void main(String args[]){
+        Scanner t=new Scanner(System.in);
+        int b=t.nextInt();
+        double res=32+(b*1.8);
+        System.out.format("%.2f
+",res);
+       
+    }
+}
