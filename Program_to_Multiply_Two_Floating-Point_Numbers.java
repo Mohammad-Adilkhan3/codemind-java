cf37996c9d4b47e4cd48fd0f4070098e2628da62
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-02-21 10:16:53 +0530

    import java.io.*; import java.util.*; public class samp{     public static void main(String args[]){         Scanner t=new Scanner(System.in);         double a=t.nextDouble();         double b=t.nextDouble();         double s=a*b;         System.out.printf("%.2f",s);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class samp{
+    public static void main(String args[]){
+        Scanner t=new Scanner(System.in);
+        double a=t.nextDouble();
+        double b=t.nextDouble();
+        double s=a*b;
+        System.out.printf("%.2f",s);
+    }
+}
