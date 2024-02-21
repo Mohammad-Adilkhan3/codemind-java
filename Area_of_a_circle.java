11c781f42c80f23eb17b27daa7728a7f90d71f93
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-02-21 09:54:47 +0530

    import java.io.*; import java.util.*; public class samp{     public static void main(String args[]){         Scanner t=new Scanner(System.in);          int n=t.nextInt();         System.out.printf("%.2f",3.14*n*n);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class samp{
+    public static void main(String args[]){
+        Scanner t=new Scanner(System.in);
+         int n=t.nextInt();
+        System.out.printf("%.2f",3.14*n*n);
+    }
+}
