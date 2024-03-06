b2454e441e1cc1a972f916ffa56db9faae409924
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-03-06 10:13:54 +0530

    import java.io.*; import java.util.*; public class samp{     public static void main(String args[]){         Scanner s=new Scanner(System.in);         int n=s.nextInt();         if(n*4>1000){             System.out.println("NO");         }         else{             System.out.println("YES");         }
        }
    }

---
+++ Tickets.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class samp{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n*4>1000){
+            System.out.println("NO");
+        }
+        else{
+            System.out.println("YES");
+        }
+        
+       
+    }
+}
