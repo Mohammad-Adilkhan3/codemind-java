0ff124dcae95a81552d3077a578d795e66ccea83
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-03-06 09:49:35 +0530

    import java.io.*; import java.util.*; public class samp{     public static void main(String args[]){         Scanner s=new Scanner(System.in);         int n=s.nextInt();         System.out.println(n*(n-1)/2);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class samp{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        System.out.println(n*(n-1)/2);
+    }
+}
