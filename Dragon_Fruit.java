29dcddf101458cbdb48730c766eec3d23024ed98
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2024-03-13 00:02:24 -0500

    import java.io.*; import java.util.*; public class samp{     public static void main(String args[]){         Scanner t=new Scanner(System.in);         int x=t.nextInt();             if(x%2==0 && x>2){             System.out.println("YES");             }              else{              System.out.println("NO");         }         }
        }

---
+++ Dragon_Fruit.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class samp{
+    public static void main(String args[]){
+        Scanner t=new Scanner(System.in);
+        int x=t.nextInt();
+            if(x%2==0 && x>2){
+            System.out.println("YES");
+            }
+             else{
+             System.out.println("NO");
+        }
+        }
+        
+        
+    }
